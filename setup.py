719eec5b91717fed7382792e86df0deeddc0f75c
JeonChaeHwan <123877184+JeonChaeHwan@users.noreply.github.com>
2024-04-20 19:49:58 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
-    name = "mycrawler",
-    packages=["mycrawler"],
+    name = "mycrawlers",
+    packages=["mycrawlers"],
     version="0.0.1",
     description="oss easily myCrawler",
     author = "ChaeHwanJeon",
